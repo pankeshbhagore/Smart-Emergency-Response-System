8a7ce49a4f88558bfe61b8397e45ea9b79831c5d
Pankesh <pankeshbhagore@gmail.com>
2026-03-10 02:44:39 +0530

    Updated

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL?.replace("/api","") || "http://localhost:5000");
+const socket = io(
+  import.meta.env.VITE_API_URL || "http://localhost:5000",
+  { transports: ["websocket","polling"] }
+);
 
 export default socket;
